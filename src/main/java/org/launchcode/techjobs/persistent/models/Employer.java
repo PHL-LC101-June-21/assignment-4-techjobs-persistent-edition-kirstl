6abf401eed2f1889a26a73a467583ee869f1417b
Kirsten <kirstenlewismmj@gmail.com>
2021-12-13 12:20:00 -0500

    "updated job model"

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -19,7 +23,15 @@ public class Employer extends AbstractEntity {
     @Size(min=1 , max=255)
     private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
     public Employer() {}
+
+    @OneToMany(mappedBy = "employer")
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
 }
 
